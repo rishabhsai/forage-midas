cac0a575b6067ad32353a830c44551a42d7c9b0a
Rishabh <rishabhsaiv@gmail.com>
2026-03-13 23:20:22 -0400

    Add balance endpoint for final task

---
+++ src/main/java/com/jpmc/midascore/controller/BalanceController.java
@@ -0,0 +1,23 @@
+package com.jpmc.midascore.controller;
+
+import com.jpmc.midascore.entity.UserRecord;
+import com.jpmc.midascore.foundation.Balance;
+import com.jpmc.midascore.repository.UserRepository;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class BalanceController {
+    private final UserRepository userRepository;
+
+    public BalanceController(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @GetMapping("/balance")
+    public Balance balance(@RequestParam Long userId) {
+        UserRecord user = userRepository.findById(userId.longValue());
+        return new Balance(user == null ? 0F : user.getBalance());
+    }
+}
